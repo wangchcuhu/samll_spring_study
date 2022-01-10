0cbed57eb6b893263eca98085a1b5ea4877f0e6d
DESKTOP-E0I6NHI\王楚 <1125258456@qq.com>
2022-01-10 17:01:32 +0800

    second

--- small_spring_study/src/main/java/step_05/core/io/Resource.java
+++
@@ -1,9 +0,0 @@
-package step_05.core.io;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-//资源加载
-public interface Resource {
-    InputStream getInputStream() throws IOException;
-}
--- small_spring_study/src/main/java/step_05/core/io/ResourcesLoader.java
+++
@@ -1,8 +0,0 @@
-package step_05.core.io;
-//统一加载资源
-public interface ResourcesLoader {
-    String CLASSPATH_URL_PREFIX = "classpath:";
-
-    Resource getRecourse(String location);
-
-}
